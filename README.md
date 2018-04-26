f5d36030c343a44441db8ff69d36461f1320adec
whhjdi <418605872@qq.com>
2018-04-26 20:52:08 +0800

    slides

---
+++ README.md
@@ -0,0 +1,3 @@
+# slides-demo
+用jQuery实现一个无缝轮播
+了解状态机的概念
---
+++ main.js
@@ -0,0 +1,40 @@
+let n 
+initState()
+setInterval(()=>{
+    makeLeave(getImage(n))
+        .one('transitionend',(e)=>{
+            makeEnter($(e.currentTarget))
+        })
+    makeCurrent(getImage(n+1))
+    n += 1
+},1000)
+
+
+function getImage(n){
+    return $(`.imgs>img:nth-child(${x(n)})`)
+}
+function x(n){
+    if(n>5){
+        n = n%5
+        if(n===0){
+            n = 5
+        }
+    }
+    return n
+}
+function initState(){
+    n = 1
+    $(`.imgs>img:nth-child(${x(n)})`).addClass('current')
+        .siblings().addClass('enter')
+}
+function makeLeave($node){
+    return $node.removeClass('current enter').addClass('leave')
+}
+function makeEnter($node){
+    return $node.removeClass('current leave').addClass('enter')
+    
+}
+function makeCurrent($node){
+    return $node.removeClass('leave enter').addClass('current')
+   
+}
